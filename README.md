6cd7a65d6b875a88d6d5114f0f8e45179f3ae4f1
twl8n <tom@laudeman.com>
2017-10-04 21:54:14 -0400

    Remove unused code. Add meaningful doc strings.

--- README.md
+++ README.md
@@ -1,136 +1,35 @@
-- todo
+#### Clojure web sql application
 
-* list-all by year, year+month
+This is a working example Clojure web application with a SQL database. The SQL is sqlite, which already exists
+on all Macs, probably most Linux distros. I suspect Windows users will have to install SQLite.
 
-* category editor
+This project uses Leiningen, which saves all kinds of headaches.
 
-* categories need to be in consistent order in list-all and edit (show) pages.
+[Leiningen for automating Clojure projects without setting your hair on fire](https://leiningen.org)
 
-* remove debug println's
+```
+> cat schema.sql| sqlite3 expmgr.db
+> lein run < /dev/null > run.log 2>&1 &
+> cat run.log
+2017-10-04 21:10:35.562:INFO:oejs.Server:jetty-7.6.8.v20121106
+2017-10-04 21:10:35.678:INFO:oejs.AbstractConnector:Started SelectChannelConnector@0.0.0.0:8080
+> jobs
+[1]  + running    lein run < /dev/null > run.log 2>&1
+> kill %1
+```
 
-x multi-category tagging
-
-x fix list-all to merge erecs with list-all-cats on the :id of each rec
-
-x check/fix that map-selected is working to create multi selected menu, which really
-should be some alternate UI, probably muliple single selection <select> elements
-
-x update db, schema and data
-
-
-x fix -main to call (ds). Now we can "lein run" and the app runs. When the app is running, point a web browser at:
+In your web browser go to:
 
 http://localhost:8080/app?action=list-all
 
-x track current year, insert not using using_year.
-
-x -6-1 causes date to be blank.
-
-x create report of sums by category
-
-select * from (select (select name from category where id=entry.category) as category,sum(amount) as sum from entry group by category) as inner order by sum;
-
-* normalize date from mm-dd to MM-DD require zero left pad.
-
-* make entry id more obvious than "Edit id 21"
-
-* list-all column sort
-
-* list-all column totals
-
-* reports by month, by year, by category.
-
-* table entry add personal/business column
-
-* row hover highlight row
-
-* edit row when click anywhere in row
-
-* row right click menu "edit,?"
-
-* shortcut navigation keys: tab, shift-tab, return shift-return
-
-* other date formats? date shortcuts?
-
-* ? break up category "equipment" into big stuff with serial number, small stuff?
-
-* Add page to insert/edit category data.
-
-* create an app, a jar file, web menu to quit, app launches browser?
-
-- Validate critical data, esp. foreign keys. Add constraint?
-
-We don't want "11 " going into an integer field that should be 11.
-
-That can cause 2 hours of confusion.
-
-Both of these work in SQLite:
-
-select entry.*,(select name from category where id='2 ') as category_name from entry where id=10;
-select entry.*,(select name from category where id=2) as category_name from entry where id=10;
-
-But the first one won't work when called from jdbc, although it returns no error message.
-
-- x insert or update all recs from list-all page.
-
-;; Nice. Works, returns new index.
-(jdbc/insert! db "entry" {:date 1 :category 2 :amount 12.3 :mileage 4 :notes "foo"})
-=>({:last_insert_rowid() 10})
-
-;; Not such a useful return value, rows effected.
-(jdbc/insert! db "entry" ["date","category","amount","mileage","notes"] ["1" "2" "3" "4" "5"])
-=>(1)
-
-;; OMG works, sth then db-do-prepared-return-keys.
-;; https://github.com/clojure/java.jdbc
-;;  sql-params is a vector containing a SQL string or PreparedStatement followed by parameters -- like other APIs in this library
-;; sql-params = \[(sql-string|preparedstatement) vector \]
-(def sth (jdbc/prepare-statement
-          (jdbc/get-connection db)
-          "insert into entry (date,category,amount,mileage,notes) values (?,?,?,?,?)"
-          {:return-keys true}))
-(jdbc/db-do-prepared-return-keys db [sth "1" "2" "3" "4" "5"])
-=>{:last_insert_rowid() 6}
-
-;; Works. Not necessary to prepare the sql, in spite of the name.
-(jdbc/db-do-prepared-return-keys db ["insert into entry (date,category,amount,mileage,notes) values (?,?,?,?,?)" "1" "2" "3" "4" "5"])
-=>{:last_insert_rowid() 7}
-
-;; IllegalArgumentException db-spec org.sqlite.PrepStmt@4534175e is missing a required parameter  clojure.java.jdbc/get-connection (jdbc.clj:338)
-(jdbc/db-do-prepared-return-keys sth ["1" "2" "3" "4" "5"])
-
-(jdbc/db-do-prepared-return-keys db true ["1" "2" "3" "4" "5"] {})
-
-;; SQLException [SQLITE_ERROR] SQL error or missing database (near "1": syntax error)  org.sqlite.DB.newSQLException (DB.java:383)
-(jdbc/db-do-prepared-return-keys db "insert into entry (date,category,amount,mileage,notes) values (?,?,?,?,?)" ["1" "2" "3" "4" "5"] {})
+Clojure web apps are web servers and use ring. Ring is a jetty server. Apache httpd is not involved, so
+Clojure apps have a fairly different architecture than LAMP Perl or PHP web apps.
 
 
-(def sth (jdbc/prepare-statement
-          (jdbc/get-connection db)
-          "insert into entry (date,category,amount,mileage,notes) values (?,?,?,?,?)"
-          {:return-keys true}))
+#### Errors
 
+```
+java.sql.SQLException: [SQLITE_ERROR] SQL error or missing database (no such table: entry)
+```
 
-(defn db-do-prepared-return-keys
-  "Executes an (optionally parameterized) SQL prepared statement on the
-  open database connection. The param-group is a seq of values for all of
-  the parameters. transaction? can be ommitted and will default to true.
-  Return the generated keys for the (single) update/insert.
-  A PreparedStatement may be passed in, instead of a SQL string, in which
-  case :return-keys MUST BE SET on that PreparedStatement!"
-  ([db sql-params]
-   (db-do-prepared-return-keys db true sql-params {}))
-  ([db transaction? sql-params]
-   (if (map? sql-params)
-     (db-do-prepared-return-keys db true transaction? sql-params)
-     (db-do-prepared-return-keys db transaction? sql-params {})))
-  ([db transaction? sql-params opts]
-   (let [opts (merge (when (map? db) db) opts)]
-     (if-let [con (db-find-connection db)]
-       (let [[sql & params] (if (sql-stmt? sql-params) (vector sql-params) (vec sql-params))]
-         (if (instance? PreparedStatement sql)
-           (db-do-execute-prepared-return-keys db sql params (assoc opts :transaction? transaction?))
-           (with-open [^PreparedStatement stmt (prepare-statement con sql (assoc opts :return-keys true))]
-             (db-do-execute-prepared-return-keys db stmt params (assoc opts :transaction? transaction?)))))
-       (with-open [con (get-connection db)]
-         (db-do-prepared-return-keys (add-connection db con) transaction? sql-params opts))))))
+You haven't created the database, or the db file is unreadable.
--- src/expense_mgr/core.clj
+++ src/expense_mgr/core.clj
@@ -11,92 +11,11 @@
   (:gen-class))
 
 
-;; use destructuring to allow a 2 arg function to work in comp
-;; Not a great example since 2 arg fun really is 1 arg with destructuring
-;; maybe partial with a true 2 arg func.
-(def fx (comp (fn [[foo bar]] (format "a: %s b: %s" foo bar)) (fn foo [xx yy] (prn xx yy) [xx yy])))
-;; (fx 1 3)
-;; 1 3
-;; "a: 1 b: 3"
-;; quick-web.core=> 
-
-
-;; A true 2 arg fn2 called via partial apply where the arg is a vector with 2 items.
-
-(defn fn2 [foo bar] (format "a: %s b: %s" foo bar))
-(def fx (comp  (partial apply fn2) (fn foo [xx yy] (prn xx yy) [xx yy])))
-;; quick-web.core=> (fx 1 3)
-;; 1 3
-;; "a: 1 b: 3"
-;; quick-web.core=> 
-
-(defn mya
-  "Loop only for side effect. The str/replace is done, but the value is only printed, not saved. stracc and
-  rex are always passed back in the recur, and only the index is modified."
-  []
-  (loop [stracc "xxxa yyya yb" 
-         rex [#"x" #"y"]
-         index 0]
-    (if (> index 100)
-      (println "hit: " index)
-      (recur 
-       (let [cs (str/replace stracc (first rex) "zz")]
-         (println index "is string: " cs)
-         stracc)
-       rex
-       (inc index)))))
-
-(defn myb
-  "Same speed as (myz). Call the body in a let binding so we can return before the index is incremented an extra time. Return with
-  index 1 unlike (mya) and (myz) which return with index 2. Loop over regex seq rex (consuming rex), modifying string
-  accumulator stracc. Use an index."
-  []
-  (loop [stracc "xxxa yyya yb" 
-         rex [#"x" #"y"]
-         index 0]
-    (let [cs (str/replace stracc (first rex) "zz")
-          rexr (rest rex)]
-      (println index "is string: " cs)
-      (if (empty? rexr)
-        (do
-          ;; This print raises the timing on 10 invocations from 33 to 47 ms.
-          (println "Final index: " index)
-          cs)
-        (recur cs 
-               rexr
-               (inc index))))))
-  
-  (defn myz
-  "Loop over regex seq rex (consuming rex), modifying string accumulator stracc. Also inc an index so we can print debug info."
-  []
-  (loop [stracc "xxxa yyya yb" 
-         rex [#"x" #"y"]
-         index 0]
-    (if (empty? rex)
-        (do
-          ;; This print raises the timing on 10 invocations from 33 to 47 ms.
-          (println "Final index: " index)
-          stracc)
-      (recur (let [cs (str/replace stracc (first rex) "zz")]
-               (println index "is string: " cs)
-               cs)
-             (rest rex) 
-             (inc index)))))
-
 (def db
   {:classname   "org.sqlite.JDBC"
    :subprotocol "sqlite"
    :subname     "expmgr.db"
    })
-
-(defn create-db []
-  (try (jdbc/db-do-commands db
-                       (jdbc/create-table-ddl :news
-                                         [:date :text]
-                                         [:url :text]
-                                         [:title :text]
-                                         [:body :text]))
-       (catch Exception e (println e))))
 
 ;; outer is a map with a key :category_list, the value of which is a list of maps with key :id.
 ;; Make a set of the :id values and if that id exists in the inner map, assoc :selected with the inner map
@@ -108,9 +27,10 @@
 ;; the set function.
 
 (defn map-selected [outer inner]
-  "Map inner and conditionally assoc :selected key, mapping the result via assoc into outer
-  as :all-category. Add key :all-category to outer, and while setting :selected appropriately on each
-  iteration of :all-category. Map assoc, map conditional assoc. And :checked with value checked."
+  "Create a data structure for categories to populate an html select element in a Clostache template. Map
+  inner and conditionally assoc :selected key, mapping the result via assoc into outer as :all-category. Add
+  key :all-category to outer, and while setting :selected appropriately on each iteration
+  of :all-category. Map assoc, map conditional assoc. And :checked with value checked."
   (map (fn [omap]
          (assoc omap :all-category
                 (map (fn [imap] 
@@ -139,13 +59,18 @@ as inner order by sum")
 ;; create table ecat as select id,category from entry;
 
 (def all-cats-sql
-  "select name as category_name,category.id from entry, category, etocat
-where 
-entry.id=etocat.eid and
-category.id=etocat.cid and entry.id=?
-order by category.id")
+"select 
+   name as category_name,category.id 
+ from entry, category, etocat
+ where 
+   entry.id=etocat.eid and
+   category.id=etocat.cid and 
+   entry.id=?
+   order by category.id")
 
-(defn list-all-cats [eid]
+(defn list-all-cats
+  "List all categories. Takes an entry id, returns a map of all category data matching the entry id."
+  [eid]
   {:category_list (jdbc/query db [all-cats-sql eid])})
 
 ;; name from category where category.id=entry.category
@@ -154,27 +79,30 @@ order by category.id")
 ;; Or can use clojure.
 
 (def show-sql
-  "select entry.*,
-(select name from category where category.id=entry.category) as category_name 
-from entry 
-where entry.id=?")
+"select entry.*,(select name from category where category.id=entry.category) as category_name 
+ from entry 
+ where entry.id=?")
 
-;; name from category where category.id=entry.category
-(defn show [params]
+(defn show
+  "Get an entry record, and get all necessary category info for the user interface."
+  [params]
   (let [id (get params "id")
         erecs (jdbc/query db [show-sql id])
         full-recs (map (fn [rec] (merge rec (list-all-cats (:id rec)))) erecs)
         cats (jdbc/query db ["select * from category order by name"])]
     (map-selected full-recs cats)))
 
-(defn choose [params]
+
+(defn choose
+  "Get a single entry record matching the title param."
+  [params]
   (let [title (params "title")]
     (when (not (nil? title))
       (jdbc/query db ["select * from entry where title like ? limit 1" (format "%%%s%%" title)]))))
 
 
 ;; using-year is a def that is an fn, and has a local, internal atom uy. Basically using-year is a closure. So
-;; that safe and good programming practice, maybe.
+;; that is safe and good programming practice, maybe.
 
 (def using-year
   (let [uy (atom "2017")]
@@ -186,7 +114,8 @@ where entry.id=?")
          @uy)))))
 
 (defn check-uy [cmap]
-  "Expect empty string for missing map values, not nil, because nil breaks re-matches."
+  "Check for a missing using-year, and default to 2017. Expect empty string for missing map values, not nil,
+  because nil breaks re-matches."
   (let [date (:date cmap)
         uy (:using_year cmap)
         full-match (re-matches #"^(\d{4})-\d{1,2}-\d{1,2}$" date)]
@@ -211,7 +140,9 @@ where entry.id=?")
           :else
           (str uy "-" date))))
 
-(defn test-smarter-date []
+(defn test-smarter-date
+  "A convenience function to validate smarter-date"
+  []
   [(smarter-date {:date "-06-01" :using_year "2001"})
    (smarter-date {:date "06-01" :using_year "2001"})])
 
@@ -243,44 +174,16 @@ where entry.id=?")
                   (prn "no id in params:" params)
                   '(0)))))
 
-(defn pq [xx] (java.util.regex.Pattern/quote xx))
 
 (defn cstr
   "Output pretty print of str. Unclear why I named this function cstr."
   [str] (str/replace (with-out-str (pprint str)) #"\n" "\n"))
 
-(defn dev-init []
-  (def myrec '({:id 1, :title "Hitachi Compact Impact Driver", :desc "The best tool I own", :stars nil, :isbn nil}))
-  (def mytpl (slurp "edit.html")))
-
-;; [string map] returning modified string
-;; (seq) the map into a sequence of k v
-;; This is the "render" of something like clostache.
-
-(defn map-re
-  "Replaced placeholders in the orig template with keys and values from the map remap. This is the functional 
-equivalent of using regexes to change a string in place."
-  [orig remap]
-  (loop [ostr orig
-         [[label value] & remainder] (seq remap)]
-    (if (nil? label)
-      ostr
-      (recur (str/replace ostr (re-pattern (pq (str "{{" label "}}"))) (str value)) remainder))))
-
-
-(def old-list-all-sql
-  "select entry.*,(select name from category where category.id=entry.category) as category_name 
-from entry
-order by entry.id")
 
 (def list-all-sql
   "select entry.*,(select name from category where category.id=entry.category) as category_name 
 from entry
 order by entry.id")
-
-
-;; (map #(assoc % :all-category cats) recs)
-;; (map #(if (= (:category %) (:id %)) (assoc % :selected 1) %)  [{:foo 1} {:foo 2}])
 
 (defn list-all [params]
   (let [erecs (jdbc/query db [list-all-sql])
@@ -304,30 +207,14 @@ order by entry.id")
                (params "notes")])]
     [{:id (get kmap (keyword "last_insert_rowid()"))}]))
 
-(defn map-re-fill-list-all
-  "Fill in a list of all records. The regex must use (?s) so that newline matches .
-Initialize with empty string, map-re on the body, and accumulate all the body strings."
-  [rseq]
-  (let [template (slurp "list-all.html")
-        [all pre body post] (re-matches #"(?s)^(.*?)\{\{for\}\}(.*?)\{\{end\}\}(.*)$" template)]
-    (str (map-re pre {:_msg ["List all from db"]})
-         (loop [full ""
-                remap rseq]
-           (prn full)
-           (if (empty? remap)
-               full
-             (recur (str full (map-re body (first remap))) (rest remap))))
-         post)))
-
 (defn fill-list-all
-  "Fill in a list of all records. The regex must use (?s) so that newline matches .
-Initialize with empty string, map-re on the body, and accumulate all the body strings."
+  "Fill in a list of all records."
   [rseq]
   (let [template (slurp "list-all.html")]
     (render template rseq)))
 
 (defn render-any
-  "Render rseq to the template file template-name."
+  "Render rseq to the template file template-name. Clostache/moustache template rendering wrapper."
   [rseq template-name]
   (let [template (slurp template-name)]
     (render template rseq)))
@@ -339,7 +226,9 @@ Initialize with empty string, map-re on the body, and accumulate all the body st
         body (render template record)]
     body))
 
-(defn request-action [working-params action]
+(defn request-action
+  "Call functions based on http request parameters."
+  [working-params action]
   (cond (= "show" action)
         (map #(assoc % :sys-msg (format "read %s from db" (get working-params "id"))) (show working-params))
         (= "choose" action)
@@ -357,7 +246,7 @@ Initialize with empty string, map-re on the body, and accumulate all the body st
         (cat-report)))
 
 (defn reply-action
-  "Generate a response for some request."
+  "Generate a response aka a reply for some http request."
   [rmap action]
   (cond (or (nil? rmap)
             (nil? (some #{action} ["show" "list-all" "insert" "update-db" "catreport"])))
@@ -393,9 +282,11 @@ Initialize with empty string, map-re on the body, and accumulate all the body st
                               {:using_year using-year
                                "date" (smarter-date {:date (or (temp-params "date") "") :using_year using-year})})
         rmap (request-action working-params action)]
-    ;; (println "action: " action "\nrmap:" rmap)
-    ;; (def aa rmap)
     (reply-action rmap action)))
+
+;; def app is ring-fu. The http request is passed through various wrappers that modify the request making it useful
+;; and easier to work with. The modifications are standard stuff in the Apache httpd world, but ala carte here
+;; in Clojure ring jetty world.
 
 (def app
   (wrap-multipart-params (wrap-params handler)))
@@ -404,62 +295,19 @@ Initialize with empty string, map-re on the body, and accumulate all the body st
 ;; example
 ;; (defonce server (run-jetty #'my-app {:port 8080 :join? false}))
 
-;; Unclear how defonce and lein ring server headless will play together.
+;; Unclear how defonce and lein ring server headless will play together. We don't much care because we only use
+;; lein run. I suspect lein ring isn't working for this app.
+
 (defn ds []
   (defonce server (ringa/run-jetty app {:port 8080 :join? false})))
 
-;; Need -main for 'lien run', but it is ignored by 'lein ring'.
+;; We need -main for 'lien run', but it is ignored by 'lein ring'.
 (defn -main []
   (ds))
 
-;; https://stackoverflow.com/questions/39765943/clojure-java-jdbc-lazy-query
-;; https://jdbc.postgresql.org/documentation/83/query.html#query-with-cursor
-;; http://clojure-doc.org/articles/ecosystem/java_jdbc/using_sql.html#exception-handling-and-transaction-rollback
-;; http://clojure-doc.org/articles/ecosystem/java_jdbc/using_sql.html#using-transactions
-
-(defn ex-lazy-select
+(defn makefresh
+  "This is a helper function when working in the repl."
   []
-  (jdbc/with-db-transaction [tx db] ;; originally connection
-    (jdbc/query tx
-                [(jdbc/prepare-statement (:connection tx)
-                                         "select * from mytable"
-                                         {:fetch-size 10})]
-                {:result-set-fn (fn [result-set] result-set)})))
-
-(defn demo-autocommit
-  "Demo looping SQL without a transaction. Every execute will auto-commit, which is time consuming. This
-  function takes 62x longer than doing these queries inside a single transaction."
-  []
-  (jdbc/execute! db ["delete from entry where title like 'demo transaction%'"])
-  (loop [nseq (range 10000)]
-    (let [num (first nseq)
-          remainder (rest nseq)]
-      (if (nil? num)
-        nil
-        (do
-          (jdbc/execute! db ["insert into entry (title,stars) values (?,?)" (str "demo transaction" num) num])
-          (recur remainder))))))
-
-(defn demo-transaction
-  "Demo looping SQL inside a transaction. This seems to lack an explicit commit, which makes it tricky to
-commit every X SQL queries. Use doall or something to un-lazy inside with-db-transaction, if you need the
-query results.
-
-http://pesterhazy.karmafish.net/presumably/2015-05-25-getting-started-with-clojure-jdbc-and-sqlite.html"
-  []
-  (jdbc/with-db-transaction [dbh db]
-    (jdbc/execute! dbh ["delete from entry where title like 'demo transaction%'"])
-    (loop [nseq (range 10000)]
-      (let [num (first nseq)
-            remainder (rest nseq)]
-        (if (nil? num)
-          nil
-          (do
-            (jdbc/execute! dbh ["insert into entry (title,stars) values (?,?)" (str "demo transaction" num) num])
-          (recur remainder)))))))
-
-
-(defn makefresh []
   (.stop server)
   (tns/refresh)
   (ds)
